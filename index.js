f4ac3a13bba6c1a62a911777d9979fd3d973fb02
Nelson Tan <nestan@uw.edu>
2017-10-22 05:45:18 -0700

    Getting heroku web

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+	if (req.query['hub.verify_token'] === 'hello world') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
