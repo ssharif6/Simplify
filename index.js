2a7bfb80df1641eb4727eafe6d03ff0abe653e90
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2017-10-22 07:25:54 -0700

    Got chatbot working

--- index.js
+++ index.js
@@ -109,15 +109,11 @@ function callAPI(userInput, sender) {
 		var response = "";		
 		var shit = "";
 		res.on('data', function(body) {
-		//   console.log('Body: ' + body);
-			// console.log(body);
 			shit += body;
-			// response += parseJson(body);
 		});
 		res.on('end', function() {
 
 			parseJson(shit, sender);
-			// sendTextMessage(sender, parseJson(shit));
 		  });
 	});
 	req.on('error', function(e) {
@@ -143,87 +139,6 @@ function parseJson(json, sender) {
 			sendTextMessage(sender, t1["body"]);
 		}
 	}
-	// responseObj.entities.forEach(function(entity) {
-	// 	var keyWord = entity.name;
-		
-	// 	// var wordDef = entity.definition;
-	// 	// var wordAndDef = "This is the definition of " + keyWord + ":\n" + wordDef;
-	// 	array.push(keyWord);
-	// }, this);
+
 	return array.join("\n");
 }
-
-// var obj = parseMessage("", "http://cdn2-www.dogtime.com/assets/uploads/gallery/shiba-inu-puppies/shiba-inu-puppy-13.jpg");
-// var x = callAPI(obj);
-// console.log(x);
-
-// var shit = parseMessage("What is linux kernel?", "");
-// var callShit = callAPI(shit, );
-// console.log(callShit);
-
-// 'use strict'
-
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const request = require('request')
-// const app = express()
-
-// app.set('port', (process.env.PORT || 5000))
-
-// // Process application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({extended: false}))
-
-// // Process application/json
-// app.use(bodyParser.json())
-
-// // Index route
-// app.get('/', function (req, res) {
-// 	res.send('Hello world, I am a chat bot')
-// })
-
-// // for Facebook verification
-// app.get('/webhook/', function (req, res) {
-// 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-// 		res.send(req.query['hub.challenge'])
-// 	}
-// 	res.send('Error, wrong token')
-// })
-
-// // Spin up the server
-// app.listen(app.get('port'), function() {
-// 	console.log('running on port', app.get('port'))
-// })
-
-// app.post('/webhook/', function (req, res) {
-//     let messaging_events = req.body.entry[0].messaging
-//     for (let i = 0; i < messaging_events.length; i++) {
-// 	    let event = req.body.entry[0].messaging[i]
-// 	    let sender = event.sender.id
-// 	    if (event.message && event.message.text) {
-// 		    let text = event.message.text
-// 		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-// 	    }
-//     }
-//     res.sendStatus(200)
-// })
-
-// const token = "EAAQOOGWd6nEBAMiZAGLXLwEveRNhPOJQNtBDnFxDC6yS31TkrEhQgOZCHbAlqwZBBMI4nav152XaJBSrj0uElg91zHczZCh3ZC9nJYi1NB8SAZAyDhZBlVTKmi1t9FvJHh3OfmZAAHJgq1XuckmGn2lEagLYJK8npK4yyQvah9lurQZDZD";
-
-// function sendTextMessage(sender, text) {
-//     let messageData = { text:text }
-//     request({
-// 	    url: 'https://graph.facebook.com/v2.6/me/messages',
-// 	    qs: {access_token:token},
-// 	    method: 'POST',
-// 		json: {
-// 		    recipient: {id:sender},
-// 			message: messageData,
-// 		}
-// 	}, function(error, response, body) {
-// 		if (error) {
-// 		    console.log('Error sending messages: ', error)
-// 		} else if (response.body.error) {
-// 		    console.log('Error: ', response.body.error)
-// 	    }
-//     })
-// }
