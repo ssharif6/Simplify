ee1e952011a59e2cb7ebd21f31516baa6d339ac3
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2017-10-21 19:56:40 -0700

    Added go src starter code

---
+++ servers/handlers/text.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "net/http"
+
+func TextHandler(w http.ResponseWriter, r *http.Request) {
+	// do something
+
+}
---
+++ servers/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"log"
+	"net/http"
+	"github.com/ssharif6/Simplify/servers/handlers"
+	"fmt"
+)
+
+func main() {
+	portAddress := os.Getenv("ADDR")
+	if len(portAddress) == 0 {
+		// default port for HTTPS
+		portAddress = ":443"
+	}
+
+	tlsKey := os.Getenv("TLSKEY")
+	tlsCert := os.Getenv("TLSCERT")
+
+	if len(tlsCert) == 0 || len(tlsKey) == 0 {
+		log.Fatalf("TLS KEY OR TLS CERT NOT SET")
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/simplify/text", handlers.TextHandler)
+	mux.HandleFunc("/v1/simplify/image", handlers.ImageHandler)
+	mux.HandleFunc("/v1/simplify/url", handlers.UrlHandler)
+
+	fmt.Printf("Listening on port %d", portAddress)
+	log.Fatal(http.ListenAndServeTLS(portAddress, tlsCert, tlsKey, mux))
+}
